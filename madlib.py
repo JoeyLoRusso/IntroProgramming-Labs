34e789eeef487b59025fd09b9ccb79570c954c5e
Joey <joey19008@gmail.com>
2018-09-06 15:19:07 -0400

    Solution to the Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,10 @@
+def main():
+    place=input("Enter a place: ")
+    nown=input("Enter a nown: ")
+    name=input("Enter a name: ")
+    verb=input("Enter a verb: ")
+    nown2=input("Enter another nown: ")
+
+    print("Tim ran to the "+place+" to see the "+nown+". When he arived, he saw his friend, "+name+". They said, 'Tim! "+verb+" to the store, they are having a sale on "+nown2+".'")
+
+main()
