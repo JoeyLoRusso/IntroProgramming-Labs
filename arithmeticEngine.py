b40fe21c0908d0af245d56d79fa645aa01cb3a98
Joey <joseph.lorusso1@marist.edu>
2018-10-25 14:38:50 -0400

    created a basic calc program

---
+++ arithmeticEngine.py
@@ -0,0 +1,49 @@
+# CMPT 120 - Lab #6
+# JOEY LORUSSO
+# 25-10-2018
+###
+
+def showIntro():
+    print("Welcome to the Arithmetic Engine!")
+    print("=================================\n")
+    print("Valid commands are 'add', 'mult', 'sub', 'div', and 'quit'.\n")
+
+def showOutro():
+    print("\nThank you for using the Arithmetic Engine…")
+    print("\nPlease come back again soon!")
+
+def doLoop():
+    while True:
+        cmd = input("What computation do you want to perform? ").lower()
+
+        def getNums():
+            num1 = int(input("Enter the first number: "))
+            num2 = int(input("Enter the second number: "))
+            return num1, num2
+        
+        if cmd == "add":
+            num1, num2 = getNums()
+            result = num1 + num2
+        elif cmd == "sub":
+            num1, num2 = getNums()
+            result = num1 - num2
+        elif cmd == "mult":
+            num1, num2 = getNums()
+            result = num1 * num2
+        elif cmd == "div":
+            num1, num2 = getNums()
+            result = num1 // num2
+        elif cmd == "quit":
+            break
+        else:
+            print(cmd, "is an invalid command")
+            continue
+
+        print("The result is " + str(result) + ".\n")
+    
+def main():
+    showIntro() #This looks good
+    doLoop()
+    showOutro()
+
+main()
